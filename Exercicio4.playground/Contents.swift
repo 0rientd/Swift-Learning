95767a0ef29ed57a2c82684c2c3db83015971569
hackerorientado <hackerorientado@protonmail.com>
2020-05-05 14:06:52 -0300

    Adicionando secao4

---
+++ Exercicio4.playground/Contents.swift
@@ -0,0 +1,12 @@
+var a = 10
+var b = 20
+
+a + b
+a - b
+a * b
+a / b
+
+a % b
+b % a
+
+
---
+++ secao4.playground/Contents.swift
@@ -0,0 +1,7 @@
+var arraysDeDoubles : [Double] = [5.0, 6.30, 8.55, 99.9, 7.2563240]
+
+arraysDeDoubles.append(7.77)
+arraysDeDoubles.append(123.123)
+arraysDeDoubles.append(147852369)
+
+arraysDeDoubles.append(Double(arraysDeDoubles.count))
