55a3cd4d8c6101d203917f8a3ef8c20a5cca6d6c
hackerorientado <hackerorientado@protonmail.com>
2020-05-08 22:05:14 -0300

    Adicionando exercicio 1 da secao 7

---
+++ secao7ex1.playground/Contents.swift
@@ -0,0 +1,19 @@
+for vezes in 1...50 {
+    print("Imprimi \(vezes) vezes")
+    
+}
+
+var numero = 2
+
+for _ in 1...5 {
+    
+    numero = numero * numero
+    print(numero)
+}
+
+var arrayDeCores : [String] = ["Vermelho", "Amarelo", "Azul", "Preto"]
+
+for cor in arrayDeCores {
+    print("A cor agora é => \(cor)")
+    
+}
